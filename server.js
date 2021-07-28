c370af5dfc0d34a9925d6d54a22b8bd160ddfba8
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-07-29 00:31:00 +0300

    Setup Express

---
+++ server.js
@@ -0,0 +1,15 @@
+const Express = require('express')
+const Path = require('path')
+
+const app = Express()
+
+const VIEWS_PATH = Path.join(__dirname, './build/')
+app.use(Express.static(VIEWS_PATH))
+
+app.get('/', (req, res) => {
+  const index = Path.join(VIEWS_PATH, 'index.html')
+  return res.sendFile(index)
+})
+
+app.listen(8080)
+
