22fd0b29e66eef49903aa9c12f801bba33584cf2
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-07-29 18:54:46 +0300

    Make it work

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
+const Seneca = require('seneca')
 const Express = require('express')
 const Path = require('path')
 
-const app = Express()
+const seneca = Seneca()
+  .use('entity')
 
-const VIEWS_PATH = Path.join(__dirname, './views/')
-app.use(Express.static(VIEWS_PATH))
+seneca.ready(() => {
+  const app = Express()
 
-app.get('/', (req, res) => {
-  const index = Path.join(VIEWS_PATH, 'index.html')
-  return res.sendFile(index)
+  app.use(Express.json())
+
+  const VIEWS_PATH = Path.join(__dirname, './views/')
+  app.use(Express.static(VIEWS_PATH))
+
+  app.get('/', (req, res) => {
+    const index = Path.join(VIEWS_PATH, 'index.html')
+    return res.sendFile(index)
+  })
+
+  app.post('/api', (req, res) => {
+    return res.json({
+      /* NOTE: meta$.empty == false is required, or else the client receives
+       * null instead of the response data.
+       */
+      meta$: { empty: false },
+
+      message: 'hello from the backend'
+    })
+  })
+
+  app.listen(8080)
 })
-
-app.listen(8080)
 
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello world!</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Seneca from 'seneca-browser'
+
+const seneca = new Seneca({
+  plugin: { browser: { endpoint: '/api' } }
+})
+
+seneca.client({
+  type: 'browser', pin: 'role:web'
+})
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  mounted() {
+    seneca.act('role:web,hello:world', function (err, out) {
+      if (err) {
+        console.error(err)
+        return
+      }
+
+      console.dir('Backend responded with: ')
+      console.dir(out)
+    })
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
